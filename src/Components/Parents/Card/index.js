1792ac945de2e05d07ea01b37a5d138ca66947f4
Brian Sunbury <bsunbury29@gmail.com>
2019-10-27 12:39:49 -0700

    edit authorization for edit do parent

--- src/Components/Parents/Card/index.js
+++ src/Components/Parents/Card/index.js
@@ -1,30 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Card, Carousel } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { ACCESS } from "../../../Constants";
+import { AuthUserContext } from '../../Authentication';
+import withAuthorization from '../../Authorization';
 
-const DogCard = ({ name, sex, breed, images }) => (
-  <Card>
-    <Carousel>
-      {images.map(image => (
-        <Carousel.Item key={image._id}>
-          <img
-            className="img img-fluid"
-            src={image.url}
-            alt={image.alt} />
-        </Carousel.Item>
-      ))}
-    </Carousel>
-    <Card.Body>
-      <Card.Title className="d-flex justify-content-between">
-        <div>{name}</div>
-        <div>{getSex(sex)}</div>
-      </Card.Title>
-      <Card.Text>{breed}</Card.Text>
-    </Card.Body>
-  </Card>
-);
 
-function getSex(sex){
-    return sex ? 'Female' : 'Male';
+const editAccess = [ACCESS.ADMIN, ACCESS.MINDFLAYER];
+const DogCard = ({ name, sex, breed, images }) => {
+  const { authUser } = useContext(AuthUserContext);
+  return (
+    <Card>
+      <Carousel>
+        {images.map(image => (
+          <Carousel.Item key={image._id}>
+            <img className="img img-fluid" src={image.url} alt={image.alt} />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+      <Card.Body>
+        <Card.Title className="d-flex justify-content-between">
+          <div>{name}</div>
+          <div>{getSex(sex)}</div>
+        </Card.Title>
+        <Card.Text>{breed}</Card.Text>
+        {withAuthorization(authUser, editAccess) && <Link>Edit</Link>}
+      </Card.Body>
+    </Card>
+  );
+};
+
+function getSex(sex) {
+  return sex ? "Female" : "Male";
 }
 
 export default DogCard;
