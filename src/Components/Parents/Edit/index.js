eb7d6d3bead360e80c02d24ad6b7d74dcef992b0
Brian Sunbury <bsunbury29@gmail.com>
2019-11-07 20:58:44 -0700

    parent edit page in progress

--- src/Components/Parents/Edit/index.js
+++ src/Components/Parents/Edit/index.js
@@ -1,19 +1,23 @@
 import React, { useContext, useState } from "react";
-import { Container, Form, Button } from "react-bootstrap";
+import { Container, Form, Button, Col, Row, Card } from "react-bootstrap";
 import withAuthorizationHOC from "../../Authorization";
 import { ACCESS } from "../../../Constants";
 import { AuthUserContext } from "../../Authentication";
 import useForm from "../../..//Hooks/useForm";
 import { makeToast } from "../../Notifications";
+import addImage from '../../../Assets/images/add_image.webp';
 
 const EditParentBase = props => {
   const [validated, setValidated] = useState(false);
   const { state } = props.location;
-  const { birthday, breed, name, sex /*_id*/ } = state;
-  const { form, handleInputChange } = useForm({
+  const { birthday, breed, name, sex, description, images /*_id*/ } = state;
+  const { form, handleInputChange, handleChange } = useForm({
     birthday,
     breed,
     name,
+    sex: sex ? 1 : 0,
+    description,
+    images,
   });
 
   function handleUpdate(event) {
@@ -48,21 +52,70 @@ const EditParentBase = props => {
             type="text"
             placeholder="Name"
           />
-          <Form.Control.Feedback type="invalid">Name is required.</Form.Control.Feedback>
+          <Form.Control.Feedback type="invalid">
+            Name is required.
+          </Form.Control.Feedback>
         </Form.Group>
         <div>{birthday}</div>
-        <Form.Group controlId="exampleForm.ControlTextarea1">
+        <Form.Group controlId="breedcontrol">
           <Form.Label>Breed</Form.Label>
           <Form.Control
             name="breed"
             value={form.breed}
             onChange={handleInputChange}
             as="textarea"
-            placeholder='Breed'
+            placeholder="Breed"
             rows="2"
           />
         </Form.Group>
-        <div>{sex ? 'Female' : 'Male'}</div>
+        <fieldset>
+          <Form.Group>
+            <Form.Label className='mr-2'>Gender</Form.Label>
+            <Form.Check
+              custom
+              inline
+              checked={form.sex===0}
+              onChange={()=>handleChange({ sex: 0 })}
+              label="male"
+              type='radio'
+              id='gender-male-radio'
+            />
+            <Form.Check
+              custom
+              inline
+              checked={form.sex===1}
+              onChange={()=>handleChange({ sex: 1 })}
+              label="female"
+              type='radio'
+              id='gender-female-radio'
+            />
+          </Form.Group>
+        </fieldset>
+        <Form.Group controlId="breedcontrol">
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            name="description"
+            value={form.description}
+            onChange={handleInputChange}
+            as="textarea"
+            placeholder="Description"
+            rows="2"
+          />
+        </Form.Group>
+        <Row className='mb-1 mt-1'>
+          {form.images.map(image =>
+            <Col xs={6} key={image._id}>
+              <EditImage 
+                src={image.url}
+                alt='doggy' />
+            </Col>  
+          )}
+          <Col>
+            <EditImage 
+              src={addImage}
+              alt='addImage' />
+          </Col>
+        </Row>
         <Button type="submit" size="sm" variant="primary">
           Update
         </Button>
@@ -85,5 +138,20 @@ const EditParentPage = props => {
     </Container>
   );
 };
+
+const EditImage = ({ src, alt }) => (
+  <Card>
+    <Card.Header className='text-right'>
+      <Button 
+        onClick={()=>console.log('todo')}
+        variant="link" 
+        className='p-0 text-danger'>Delete</Button>
+    </Card.Header>
+    <Card.Img 
+      variant="bottom" 
+      src={src} 
+      alt={alt} />
+  </Card>
+);
 
 export default EditParentPage;
