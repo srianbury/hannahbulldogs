c5c0823a9529fee7b4427783293ef67197028939
Brian Sunbury <bsunbury29@gmail.com>
2019-11-16 16:35:38 -0700

    Updates

---
+++ precache-manifest.ed1650f761cda2c187bc9d686f126e0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93e407a9a44f13a1e63a452b24627f4c",
+    "url": "/hannahbulldogs/index.html"
+  },
+  {
+    "revision": "cd37fd0feb73a8e992bc",
+    "url": "/hannahbulldogs/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "6d39eac5615e6978fd07",
+    "url": "/hannahbulldogs/static/css/main.7fe38a2f.chunk.css"
+  },
+  {
+    "revision": "cd37fd0feb73a8e992bc",
+    "url": "/hannahbulldogs/static/js/2.6a65da60.chunk.js"
+  },
+  {
+    "revision": "6d39eac5615e6978fd07",
+    "url": "/hannahbulldogs/static/js/main.131ddc16.chunk.js"
+  },
+  {
+    "revision": "01407517e879a43c6a5b",
+    "url": "/hannahbulldogs/static/js/runtime-main.ee8c205f.js"
+  },
+  {
+    "revision": "7c83ec83f7e9574282e3054e2718c1ba",
+    "url": "/hannahbulldogs/static/media/add_image.7c83ec83.webp"
+  }
+]);
