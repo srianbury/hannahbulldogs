fe6d51a09d0ff98aa732092019a5350d743f9563
Brian Sunbury <bsunbury29@gmail.com>
2019-11-16 14:00:46 -0700

    home

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './header.js';
 import { AboutBrief } from '../About';
 import { GalleryPreview } from '../Gallery';
+import withLoading from '../Loading/withLoading';
 
-const Home = () => (
+const Home = () => {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    async function read() {
+      const response = await fetch(
+        `http://localhost:4020/api/dev/home`,
+      );
+      const result = await response.json();
+      setData(result.data);
+    }
+    read();
+  }, []);
+  return (
     <>
-        <Header />
-        <GalleryPreview />
-        <AboutBrief />
+      <Header />
+      <InfoWithLoading loading={data} data={data} />
     </>
+  );
+};
+
+const Info = ({ data }) => (
+  <>
+    <GalleryPreview puppies={data.puppies} />
+    <AboutBrief about={data.about} />
+  </>
 );
+const InfoWithLoading = withLoading(Info);
 
 export default Home;
