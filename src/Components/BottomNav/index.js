15f289191a077d5dadcc843f701dcc0fe11c47f5
Brian Sunbury <bsunbury29@gmail.com>
2019-11-30 13:19:52 -0700

    changed links on nav icons

--- src/Components/BottomNav/index.js
+++ src/Components/BottomNav/index.js
@@ -1,32 +1,26 @@
-import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 
 const BottomNav = () => (
   <Navbar
-    bg='light'
-    variant='light' 
-    className='mt-2'>
-    <Nav className="mr-auto" />
-    <Nav>
-        <a className='ml-2'
-            href='https://www.instagram.com/stanleybulldogs/'
-            target='_blank'
-            rel='noopener noreferrer'>
-            <i className="fab fa-instagram fa-2x text-danger"></i>
-        </a>
-        <a className='ml-2'
-            href="sms:+1-480-310-0538"
-            target='_blank'
-            rel='noopener noreferrer'>
-            <i className="fas fa-phone fa-2x text-danger"></i>
-        </a>
-        <a className='ml-2'
-            href="sms:+1-480-310-0538"
-            target='_blank'
-            rel='noopener noreferrer'>
-            <i className="fas fa-envelope fa-2x text-danger"></i>
-        </a>
-    </Nav>
+    bg="light"
+    variant="light"
+    className="d-flex justify-content-around mt-2"
+  >
+    <a
+      className="ml-2"
+      href="https://www.instagram.com/stanleybulldogs/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <i className="fab fa-instagram fa-2x text-danger"></i>
+    </a>
+    <a className="ml-2" href="sms:+1-480-310-0538">
+      <i className="fas fa-phone fa-2x text-danger"></i>
+    </a>
+    <a className="ml-2" href="mailto:rsunbur@msn.com">
+      <i className="fas fa-envelope fa-2x text-danger"></i>
+    </a>
   </Navbar>
 );
 
