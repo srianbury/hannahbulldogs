396f20c73892da9b4ae5cee0fd4be3832b1692d8
Brian Sunbury <bsunbury29@gmail.com>
2019-12-08 22:31:03 -0700

    context to update litter after update, moved read litter to context, moved read home to context so it can be reloaded after its updated

--- src/Components/Context/index.js
+++ src/Components/Context/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import DataContext from './context';
 import { DATA } from '../../Constants';
+import sentryLogger from '../../Functions/Logger';
 
 const DataContextWrapper = ({ children }) => {
   const [data, setData] = useState({});
@@ -20,6 +21,17 @@ const DataContextWrapper = ({ children }) => {
     updateNode(DATA.PARENTS, listWithUpdatedData);
   }
 
+  function updateLitters(updatedData) {
+    const currentLitterList = data[DATA.PUPPIES];
+    const listWithUpdatedData = currentLitterList.map(litter => {
+      if (litter._id === updatedData._id) {
+        return { ...litter, ...updatedData };
+      }
+      return litter;
+    });
+    updateNode(DATA.PUPPIES, listWithUpdatedData);
+  }
+
   function getParent(id) {
     const currentParentList = data.parents;
     const parentAtId = currentParentList.find(
@@ -28,13 +40,51 @@ const DataContextWrapper = ({ children }) => {
     return parentAtId;
   }
 
+  function getLitter(id) {
+    const currentLitterList = data[DATA.PUPPIES];
+    const litterAtId = currentLitterList.find(
+      litter => litter._id === id,
+    );
+    return litterAtId;
+  }
+
+  async function readParents(onError) {
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}/dogs`,
+      );
+      const result = await response.json();
+      updateNode(DATA.PARENTS, result.data);
+    } catch (e) {
+      sentryLogger(e);
+      onError();
+    }
+  }
+
+  async function readHome(onError) {
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}/home`,
+      );
+      const result = await response.json();
+      updateNode(DATA.HOME, result.data);
+    } catch (e) {
+      sentryLogger(e);
+      onError(new Error('Failed to fetch.'));
+    }
+  }
+
   return (
     <DataContext.Provider
       value={{
         data,
         updateNode,
         updateParents,
+        updateLitters,
         getParent,
+        getLitter,
+        readParents,
+        readHome,
       }}
     >
       {children}
